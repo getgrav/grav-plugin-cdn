da6f2c3b5b97375b3353543f003675802bead851
Djamil Legato <djamil@djamil.it>
2015-10-23 09:21:56 -0700

    Reverted CDN to proper regex fix commit that got lost after release

--- cdn.php
+++ cdn.php
@@ -1,7 +1,7 @@
 <?php
 namespace Grav\Plugin;
 
-use \Grav\Common\Plugin;
+use Grav\Common\Plugin;
 
 class CdnPlugin extends Plugin
 {
@@ -37,28 +37,56 @@ class CdnPlugin extends Plugin
     public function onOutputGenerated()
     {
         $config = $this->grav['config']->get('plugins.cdn');
+        $format = $this->grav['uri']->extension() ?: 'html';
+        // only process for HTML pages
+        if (!in_array($format, (array) $config['valid_formats'])) {
+            return;
+        }
 
         $cache = $this->grav['cache'];
-        $key = '?' . $cache->getKey();
+        $key   = '?' . $cache->getKey();
 
-        $pullzone = 'http://'.$config['pullzone'];
-        $base = str_replace('/', '\/', $this->grav['base_url_relative']);
+        $pullzone   = 'http://' . $config['pullzone'];
+        $base       = str_replace('/', '\/', $this->grav['base_url_relative']);
         $extensions = $config['extensions'];
-        $tag_attributes = $config['tag_attributes'];
-        $tags = $config['tags'];
-        $replace = '$1'.$pullzone.'$2"';
+        $tags       = $config['tags'];
 
-        $skip_fail = "(?:<pre[^<]*>(?:\n|.)*<\/pre>|<code[^<]*>(?:\n|.)*<\/code>)(*SKIP)(*F)|";
+        // match all pre/code blocks
+        preg_match_all("/<(pre|code)((?:(?!<\/\\1).)*?)<\/\\1>/uis", $this->grav->output, $blocks);
 
-        $regex = "/".$skip_fail."((?:<(?:".$tags.")\b)[^>]*?(?:href|src)=\")(?:(?!\/{2}))(?:".$base.")(.*?\.(?:".$extensions.")(?:(?!(?:\?|&)nocdn).*?))(?<!(\?|&)nocdn)\"/i";
+        $regex = "/((?:<(?:" . $tags . ")\b)[^>]*?(?:href|src)=\")(?:(?!\/{2}))(?:" . $base . ")(.*?\.(?:" . $extensions
+            . ")(?:(?!(?:\?|&)nocdn).*?))(?<!(\?|&)nocdn)\"/i";
 
-        $this->grav->output = preg_replace($regex, $replace, $this->grav->output);
+        $this->grav->output = preg_replace_callback(
+            $regex,
+            function ($matches) use ($blocks, $pullzone) {
+                $isBlock = $this->array_search_partial($blocks[0], $matches[0]);
+                return $isBlock ? $matches[0] : $matches[1] . $pullzone . $matches[2] . '"';
+            },
+            $this->grav->output
+        );
 
         // replacements for inline CSS url() style references
         if ($config['inline_css_replace']) {
-            $replace = '$1'.$pullzone.'$2';
-            $regex = "/".$skip_fail."(url\()(?:".$base.")(.*?\.(?:".$extensions."\)))/i";
-            $this->grav->output = preg_replace($regex, $replace, $this->grav->output);
+            $regex = "/(url\()(?:" . $base . ")(.*?\.(?:" . $extensions . "\)))/i";
+
+            $this->grav->output = preg_replace_callback(
+                $regex,
+                function ($matches) use ($blocks, $pullzone) {
+                    $isBlock = $this->array_search_partial($blocks[0], $matches[0]);
+                    return $isBlock ? $matches[0] : $matches[1] . $pullzone . $matches[2] . '"';
+                },
+                $this->grav->output
+            );
+        }
+    }
+
+    private function array_search_partial($arr, $keyword)
+    {
+        foreach ($arr as $index => $string) {
+            if (strpos($string, $keyword) !== false) {
+                return $index;
+            }
         }
     }
 }
